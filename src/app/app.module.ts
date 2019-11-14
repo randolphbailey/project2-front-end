d0dd617fa28a1a3987e0e455fabaf91142582212
randolphbailey <randolphbailey@gmail.com>
2019-11-13 20:22:13 -0600

    refactor.  remove Test folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { LoginComponent } from "./components/login/login.component";
-import { RegisterComponent } from "./components/register/register.component";
-//import { HomepageComponent } from './homepage/homepage.component';
 import { ProfileComponent } from "./components/profile/profile.component";
 import { NewpostComponent } from "./components/newpost/newpost.component";
 import { PostComponent } from "./components/post/post.component";
@@ -14,8 +11,6 @@ import { PostComponent } from "./components/post/post.component";
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     ProfileComponent,
     NewpostComponent,
     PostComponent
